70c8cfd36ba4da478e5f41a3366ba701d72b8622
Aleksandr Savchenko <titamoto@gmail.com>
2023-04-02 14:01:13 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(driversNames, string) {
+return driversNames.filter(name => name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(driversNames, string) {
+    return driversNames.filter(name => name.startsWith(string));
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string);
+}
